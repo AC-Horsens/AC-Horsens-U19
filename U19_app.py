119c601a7f37b425c09117e00094b710c624d502
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-07-23 11:59:24 +0200

    Update U19_app.py

--- U19_app.py
+++ U19_app.py
@@ -699,7 +699,7 @@ def training_ratings():
 
     # Drop rows where Rating is NaN
     filtered_df = filtered_df.dropna(subset=['Rating'])
-
+    filtered_df['Rating'] = filtered_df['Rating'].astype(float)
     # Ensure 'Rating' column is numeric
     average_ratings = filtered_df.groupby(['date', 'Player']).agg({'Rating': 'mean'}).reset_index()
 
