bb9f024073fa32584d05f0f073e86707b7b36de9
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-07-23 11:55:33 +0200

    Update U19_app.py

--- U19_app.py
+++ U19_app.py
@@ -668,7 +668,7 @@ def training_ratings():
     df['date'] = df['Tidsstempel'].dt.strftime('%d/%m/%Y')
 
     # Melt the DataFrame to long format
-    df_melted = df.melt(id_vars=['Tidsstempel', 'Coach name', 'date'], 
+    df_melted = df.melt(id_vars=['Coach name', 'date'], 
                         var_name='Player', 
                         value_name='Rating')
 
