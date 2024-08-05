017e9d683ecfffe409b4b340bafc72525498c4c0
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-08-05 13:51:42 +0200

    Update U19_app.py

--- U19_app.py
+++ U19_app.py
@@ -872,9 +872,9 @@ def player_data(events,df_matchstats,balanced_central_defender_df,fullbacks_df,n
 
 def dashboard(events):
     st.title('U19 Dashboard')
+    events['label'] = events['label'] + ' ' + events['date']
     events['date'] = pd.to_datetime(events['date'])
     events = events.sort_values('date').reset_index(drop=True)
-    events['label'] = events['label'] + ' ' + events['date']
     matches = events['label'].unique()
     matches = matches[::-1]
     match_choice = st.multiselect('Choose a match', matches)
