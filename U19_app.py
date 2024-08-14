7b5abbf603da5869c8038d05e72863c474e4930f
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-08-14 14:57:24 +0200

    Update U19_app.py

--- U19_app.py
+++ U19_app.py
@@ -996,6 +996,7 @@ def dashboard():
     df_possession_stats = df_possession_stats.rename(columns={'count':'terr_Possession'})
     df_possession_stats = df_possession_stats.rename(columns={'territorial_possession':'team.name'})
     df_possession_stats['team.name'] = df_possession_stats['team.name'].apply(lambda x: x if x == 'Horsens U19' else 'Opponent')
+    dangerzone_entries = dangerzone_entries.value_counts(['team.name','label']).reset_index()
     st.dataframe(dangerzone_entries)
     team_summary = df_xg_summary.merge(df_passes, on=['team.name','label'])
     team_summary = team_summary.merge(penareaentries, on=['team.name','label'])
