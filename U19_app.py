173221bae3111d37bac80bf1594f2982739d8549
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-08-14 14:34:17 +0200

    Update U19_app.py

--- U19_app.py
+++ U19_app.py
@@ -980,7 +980,7 @@ def dashboard():
 
     df_xg_summary = df_xg.groupby(['team.name','label'])['shot.xg'].sum().reset_index()
     df_ppda = df_ppda[df_ppda['label'].isin(match_choice)]
-    df_ppda = df_ppda.groupby(['team.name','label']).sum().reset_index()
+    df_ppda = df_ppda.groupby(['team.name','label'])['PPDA'].sum().reset_index()
     df_ppda = df_ppda.drop(columns=['date'],errors = 'ignore')
     df_ppda = df_ppda['team.name'] = df_ppda['team.name'].apply(lambda x: x if x == 'Horsens U19' else 'Opponent')
     penareaentries = penareaentries.groupby(['team.name','label']).sum().reset_index()
