2a234e4c889c48eededc33e6cab12d26b0e410c3
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-08-14 09:29:13 +0200

    Update U19_app.py

--- U19_app.py
+++ U19_app.py
@@ -1175,7 +1175,7 @@ def dashboard():
             team_data = ppda[ppda['team.name'] == team]
             line_size = 5 if team == 'Horsens U19' else 1  # Larger line for Horsens
             fig.add_trace(go.Scatter(
-                x=team_data['label'], 
+                x=team_data['date'], 
                 y=team_data['PPDA'], 
                 mode='lines',
                 name=team,
