d5212367873b9b6df7e31585ccd9ab40efe77c65
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-08-21 08:27:09 +0200

    Update U19_app.py

--- U19_app.py
+++ U19_app.py
@@ -1290,6 +1290,7 @@ def dashboard():
         st.pyplot(fig)
         player_dangerzone_received = player_dangerzone_received.rename(columns={'pass.recipient.name': 'player.name','dangerzone_entry': 'dangerzone_received'})
         player_dangerzone_entries = player_dangerzone_entries.merge(player_dangerzone_received)
+        st.dataframe(player_dangerzone_entries,hide_index=True)
         player_dangerzone_entries['Total'] = player_dangerzone_entries['dangerzone_entry'] + player_dangerzone_entries['dangerzone_received']
         player_dangerzone_entries = player_dangerzone_entries.sort_values('Total', ascending=False)
         st.dataframe(player_dangerzone_entries,hide_index=True)
