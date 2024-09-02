f190ff0e2ad4b1e7567005eb1ef14c8d6e316346
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-09-02 09:06:42 +0200

    Update U19_app.py

--- U19_app.py
+++ U19_app.py
@@ -288,7 +288,7 @@ def Process_data_spillere(events,df_xg,df_matchstats,groundduels):
         
         df_sekser['Defending'] = df_sekser[['percent_duelsWon score','opponents xg score','totalDuels score','stoppedProgressPercentage score','stoppedProgressPercentage score','recoveredPossessionPercentage score','average_interceptions score','average_interceptions score','ballRecovery score']].mean(axis=1)
         df_sekser['Passing'] = df_sekser[['percent_successfulPasses score','percent_successfulPasses score']].mean(axis=1)
-        df_sekser['Progressive ball movement'] = df_sekser[['average_shotAssists score','average_progressivePasses score','average_forwardPasses score','average_successfulForwardPasses score','percent_successfulPassesToFinalThird score','Possession value total score']].mean(axis=1)
+        df_sekser['Progressive ball movement'] = df_sekser[['average_shotAssists score','average_progressivePasses score','average_forwardPasses score','average_successfulForwardPasses score','percent_successfulPassesToFinalThird score']].mean(axis=1)
         df_sekser['Possession value added'] = df_sekser[['average_successfulPassesToFinalThird score','average_progressivePasses score','average_progressivePasses score','percent_successfulPassesToFinalThird score','percent_successfulProgressivePasses score','percent_successfulProgressivePasses score']].mean(axis=1)
         
         df_sekser = calculate_score(df_sekser, 'Defending', 'Defending_')
