3b853c580dfeae7e75887c89cba76246ea2be833
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-09-11 10:19:42 +0200

    Update U19_app.py

--- U19_app.py
+++ U19_app.py
@@ -1549,7 +1549,7 @@ def opposition_analysis():
         'total_forwardPasses', 'total_successfulForwardPasses', 'total_longPasses',
         'total_recoveries', 'total_opponentHalfRecoveries', 'total_losses',
         'total_ownHalfLosses', 'total_touchInBox', 'total_progressivePasses',
-        'total_counterpressingRecoveries','forward pass share','long pass share'
+        'total_counterpressingRecoveries'
     ]
 
 # Create "per match" columns by dividing by 'label'
