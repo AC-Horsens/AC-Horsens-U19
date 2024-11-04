6708c8364381b80f401b34077a72c458642b750f
Seamus1992 <spbartholdy@gmail.com>
2024-11-04 11:55:18 +0100

    Update U19_app.py

--- U19_app.py
+++ U19_app.py
@@ -1487,7 +1487,7 @@ def opposition_analysis():
     # Correct date format in 'date' column
     df_matchstats['date'] = df_matchstats['date'].str.replace(r'GMT\+(\d)$', r'GMT+0\1:00', regex=True)
     df_PPDA['date'] = df_PPDA['date'].str.replace(r'GMT\+(\d)$', r'GMT+0\1:00', regex=True)
-    
+
     # Group by and aggregate match stats
     df_matchstats = df_matchstats.groupby(['team.name', 'label', 'date']).sum().reset_index()
     df_matchstats = df_matchstats.merge(df_PPDA, on=['team.name', 'label', 'date'], how='left')
@@ -1502,8 +1502,9 @@ def opposition_analysis():
     df_matchstats = df_matchstats.dropna(subset=['date'])
 
     # Ensure all datetime objects are timezone-naive (remove timezone if present)
-    df_matchstats['date'] = df_matchstats['date'].apply(lambda x: x.tz_convert(None) if x.tzinfo else x)
-
+    df_matchstats['date'] = df_matchstats['date'].apply(
+        lambda x: x.tz_convert(None) if x.tzinfo else x
+    )
 
     # Drop rows where date parsing failed (NaT)
     df_matchstats['date'] = df_matchstats['date'].astype(str)
