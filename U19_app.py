571f01df5abc91623d18128f10bd36fadfb74073
Seamus1992 <spbartholdy@gmail.com>
2024-11-04 13:25:44 +0100

    Update U19_app.py

--- U19_app.py
+++ U19_app.py
@@ -1504,7 +1504,7 @@ def opposition_analysis():
     df_matchstats['date'] = df_matchstats['date'].astype(str)
     df_matchstats['date'] = df_matchstats['date'].str.slice(0, -9)
     date_format = '%Y-%m-%d'
-    df_matchstats['date'] = pd.to_datetime(df_matchstats['date'], format=date_format)
+    #df_matchstats['date'] = pd.to_datetime(df_matchstats['date'], format=date_format)
     min_date = df_matchstats['date'].min()
     max_date = df_matchstats['date'].max()
 
